c949fb9f5ff3f8f21fcd1eebb711e569ff19cae9
Jordan Ring-Sakabe <jordanringsakabe@gmail.com>
2022-12-22 16:09:44 -0700

    completed_next_lesson

--- practice-app/src/app/app.module.ts
+++ practice-app/src/app/app.module.ts
@@ -1,12 +1,20 @@
+// modules.ts files bundles the different pieces of an app into packages.
+// a module is a bundle of functionalities of an app and features for the AppComponent.
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
+//this is the NgModule decorator. Decorators are a design pattern that is used to separate modification or decoration of a class without modifying the original source code.
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent],
+
   imports: [BrowserModule, AppRoutingModule],
+
   providers: [],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- practice-app/src/app/server/server.component.ts
+++ practice-app/src/app/server/server.component.ts
@@ -4,6 +4,4 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {
-  constructor(parameters) {}
-}
+export class ServerComponent {}
